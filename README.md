85e71ed11f4c87267876314edc7e993e6707de12
Gabouchet <gabriel.hamel.pro@gmail.com>
2021-06-09 22:55:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Dictionary
+
+## Tools
+- GCC
+- CMake
+
+## Build
+
+Create build folder
+```bash
+mkdir build
+cd build
+```
+
+Launch build with cmake executable
+```bash
+cmake ..
+cmake --build .
+```
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Gabriel Hamel (gabriel.hamel.pro@gmail.com)
+ * @brief Program index
+ * @version 1.0
+ * @date 2021-06-09
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <iostream>
+
+int main(int ac, const char **av)
+{
+    std::cout << "Hello world" << std::endl;
+}
